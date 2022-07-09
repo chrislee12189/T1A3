361c8ab2db6a8f5d7d9408412e10588d13a217fb
chrislee12189 <12189@coderacademy.edu.au>
2022-07-09 15:19:44 +1000

    commit3

--- main2.py
+++ main2.py
@@ -8,16 +8,30 @@ def action():
     action = input("What would you like to do?: ")
     return action
 #------------------------------------------------------------------------------------------#
-                                #FUNCTIONAL AS OF COMMIT 2
+                                #FUNCTIONAL AS OF COMMIT 3
 movie_list = ['Haunted house', 'The ritual', 'Stalker', 'The kidnapping', 'Torture']
 
 actions= action()
 if actions == "random":
     # movie_list = ['Haunted house', 'The ritual', 'Stalker', 'The kidnapping', 'Torture']
     x = random.choice(movie_list)
+    print("Watch this movie: ")
     print(x)
 
 if actions == "check":
     print(movie_list)
 
+if actions == "rent":
+    print("Enter your card information")
+    while True:
+        try:
+            card_info = int(input("Please enter card information: "))
+        except ValueError:
+            print("Sorry, i didnt understand that. Enter numbers only")
+        # continue 
+        else:
+            #input is correct, can continue
+            break
+    print(f"You entered: {card_info}")
 #------------------------------------------------------------------------------------------#
+
